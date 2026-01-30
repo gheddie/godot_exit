921d4a91729200924ae40db1be2234f3174587da
gheddie <stefan.schulz.1976@googlemail.com>
2026-01-30 18:18:29 +0100

    further work

--- assets/collectable/simplebox/SimpleBoxCollectable.gd
+++ assets/collectable/simplebox/SimpleBoxCollectable.gd
@@ -2,13 +2,16 @@ class_name SimpleBoxCollectable
 extends BaseCollectable
 
 func getMaxCollectableImpact() -> int:
-	return 28
+	return 67
 
 func getHarvestingTesholds() -> Array[int]:
-	return [12,7]
+	return [48,25,17,9]
 
 func registerElapsableParts(harvestingDescriptor: HarvestingDescriptor) -> void:
-	pass
+	harvestingDescriptor.registerElapsableParts(48, ["CoverSlice"], self)
+	harvestingDescriptor.registerElapsableParts(25, ["Cover"], self)
+	harvestingDescriptor.registerElapsableParts(17, ["TopBreak3","TopBreak4","TopBreak7","TopBreak12"], self)
+	harvestingDescriptor.registerElapsableParts(9, ["TopBreak5","TopBreak6","TopBreak9","TopBreak18","TopBreak19"], self)
 
 func getDescriptor() -> String:
 	return "SimpleBox"
